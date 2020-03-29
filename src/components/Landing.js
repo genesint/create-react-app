8a25605da7e787712b728efa1294c2c0142984fd
John Kyeyune <genesint@gmail.com>
2020-03-29 14:27:14 +0300

    updated as per the site

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,11 +5,13 @@ import Grid from '@material-ui/core/Grid';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
+import Link from '@material-ui/core/Link';
 import Image from 'material-ui-image';
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
+    width: "95%"
   },
   paper: {
     padding: theme.spacing(2),
@@ -49,12 +51,14 @@ export default function Landing() {
         <Grid item xs={1}></Grid>
         <Grid item xs={10}>
           <Paper className={classes.paper} elevation={0}>
-            <Image
-              animationDuration={5000}
-              style={{ width: "100%" }}
-              src="https://test.avantgardedistributors.com/web/image/product.product/2769/image"
-              onClick={() => console.log('onClick')}
-            />
+            <Link href="https://avantgardedistributors.com/services" >
+              <Image
+                animationDuration={5000}
+                style={{ width: "100%" }}
+                src="https://test.avantgardedistributors.com/web/image/product.product/2769/image"
+                onClick={() => console.log('onClick')}
+              />
+            </Link>
           </Paper>
         </Grid>
         <Grid item xs={1}></Grid>
@@ -74,22 +78,27 @@ const tileData = [
   {
     img: "https://test.avantgardedistributors.com/web/image/product.product/2769/image",
     title: 'Pumps',
-    author: 'author',
+    url: 'https://avantgardedistributors.com/shop/category/water-division-pumps-1?order=name+asc',
+  },
+  {
+    img: "https://test.avantgardedistributors.com/web/image/product.product/2769/image",
+    title: 'Hoses',
+    url: 'https://avantgardedistributors.com/shop/category/water-division-hoses-3?order=name+asc',
+  },
+  {
+    img: "https://test.avantgardedistributors.com/web/image/product.product/2769/image",
+    title: 'Fittings & Accessories',
+    url: 'https://avantgardedistributors.com/shop/category/water-division-fittings-accessories-6?order=name+asc',
   },
   {
     img: "https://test.avantgardedistributors.com/web/image/product.product/2769/image",
     title: 'Generators',
-    author: 'author',
+    url: 'https://avantgardedistributors.com/shop/category/water-division-generators-4?order=name+asc',
   },
   {
     img: "https://test.avantgardedistributors.com/web/image/product.product/2769/image",
-    title: 'Pipes',
-    author: 'author',
-  },
-  {
-    img: "https://test.avantgardedistributors.com/web/image/product.product/2769/image",
-    title: 'Others',
-    author: 'author',
+    title: 'Spares',
+    url: 'https://avantgardedistributors.com/shop/category/water-division-spares-5?order=name+asc',
   },
 ];
 
@@ -100,13 +109,12 @@ function SingleLineGridList() {
     <div classes={{
       root: classes.gridRoot
     }}>
-      <GridList className={classes.gridList} cols={4}>
+      <GridList className={classes.gridList} cols={5}>
         {tileData.map((tile) => (
-          <GridListTile key={tile.title}>
+          <GridListTile key={tile.title} component={Link} href={tile.url}>
             <Image
               animationDuration={5000}
               src={tile.img}
-              onClick={() => console.log('onClick')}
               disableSpinner
             />
             <GridListTileBar
