923de14ae7a18e3f7d9d935320705f272787d550
John Kyeyune <genesint@gmail.com>
2020-04-01 23:29:41 +0300

    update

--- src/components/FrontCarousel.js
+++ src/components/FrontCarousel.js
@@ -3,11 +3,9 @@ import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-import Image from 'material-ui-image';
 
 const useStyles = makeStyles(theme => ({
     root: {
