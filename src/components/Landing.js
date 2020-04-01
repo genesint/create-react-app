943a51d31246bd724f8e3976e87c22d833c1c035
John Kyeyune <genesint@gmail.com>
2020-04-01 23:58:12 +0300

    update

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import useSWR from 'swr';
 import FrontCarousel from "./FrontCarousel";
 
 const useStyles = makeStyles((theme) => ({
-
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
   progress: {
     display: 'flex',
     '& > * + *': {
@@ -30,13 +39,21 @@ const LoadingFrontPage = () => {
   );
 }
 export default function Landing() {
+  const classes = useStyles();
   const { data, error } = useSWR('path', fetchConfig);
   if (error) return <div>failed to load</div>
   if (!data) return <LoadingFrontPage />
   return (
-    <React.Fragment>
-      <FrontCarousel slides={data.tiles} />
-    </React.Fragment>
+    <div className={classes.root}>
+      <Grid container spacing={1}>
+        <Grid item xs={1}></Grid>
+        <Grid item xs={10}>
+          <Paper className={classes.paper} elevation={0}>
+            <FrontCarousel slides={data.tiles} /></Paper>
+        </Grid>
+        <Grid item xs={1}></Grid>
+      </Grid>
+    </div>
   );
 }
 
